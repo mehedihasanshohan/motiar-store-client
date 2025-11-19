0c72e7e38ffa880092e3df4e5f9ee7bf2a65c366
mehedi hasan <mhshohan04@gmail.com>
2025-11-19 10:05:46 +0600

    projects setup with vite + tailwindcss + daisyui + react router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button className='btn btn-accent'>hello</button>
+        <h2 className='text-red-400 font-extrabold'>Mehedi hasan shohan</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
