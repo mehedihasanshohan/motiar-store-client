2b7e8172fb546fad72edf7cc812e1efd969d93f1
mehedi hasan <mhshohan04@gmail.com>
2025-11-23 15:25:41 +0600

    added login page with firebase login and use hook validation

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import useAuth from '../../hooks/useAuth';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState:{errors}
+  } = useForm();
+  const {signInUser} = useAuth();
+
+  const handleLogin = (data) => {
+    console.log(data);
+    signInUser(data.email, data.password)
+    .then(result => {
+      console.log(result.user);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <fieldset className="fieldset">
+          {/* email */}
+          <label className="label">Email</label>
+          <input type="email" {...register('email', {required: true})} className="input" placeholder="Email" />
+          {errors.email?.type === 'required' && <p className='text-red-500 font-semibold'>Email is required</p>}
+
+          {/* password */}
+          <label className="label">Password</label>
+          <input type="password" {...register('password', {
+            required: true,
+            minLength: 6,
+            pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/
+            })} className="input" placeholder="Password" />
+          {
+            errors.password?.type === "required" &&
+             <p className='text-red-500 font-semibold'>Password is required</p>
+          }
+          {
+            errors.password?.type === "minLength" &&
+            <p className='text-red-400 font-semibold'>Password must be 6 correcter or longer</p>
+          }
+          {
+            errors.password?.type === "pattern" &&
+            <p className='text-red-500'>Password must be at least one
+            uppercase, at least one lowercase, at least one number and at least one special characters</p>
+          }
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
   )
 }
 
