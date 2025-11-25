1a609c78283c97cda1f926ac2cc1b32b2adb9c2d
mehedi hasan <mhshohan04@gmail.com>
2025-11-25 22:15:17 +0600

    Delete Parcel Operation & refresh page by Tanstack Refetch()

--- src/pages/dashboard/MyParcel.jsx
+++ src/pages/dashboard/MyParcel.jsx
@@ -1,61 +1,92 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import useAuth from '../../hooks/useAuth'
-import useAxiosSecure from '../../hooks/useAxiosSecure';
-import { FiEdit } from 'react-icons/fi';
-import { PiFileMagnifyingGlass } from 'react-icons/pi';
-import { FaTrashCan } from 'react-icons/fa6';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FiEdit } from "react-icons/fi";
+import { PiFileMagnifyingGlass } from "react-icons/pi";
+import { FaTrashCan } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const MyParcel = () => {
-  const {user}= useAuth();
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const {data: parcels = [] } = useQuery({
-    queryKey: ['myParcel', user?.email],
+  const { data: parcels = [], refetch } = useQuery({
+    queryKey: ["myParcel", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
       return res.data;
-    }
-  })
+    },
+  });
+
+  const handleParcelDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          console.log(res.data);
+
+          if (res.data.deletedCount) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel request has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <h2>All of my parcels: {parcels.length}</h2>
       <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Cost</th>
-        <th>Payment Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        parcels.map((parcel, index)=>  <tr>
-        <th>{index + 1}</th>
-        <td>{parcel.parcelName}</td>
-        <td>{parcel.cost}</td>
-        <td>
-          <button className='btn btn-square hover:bg-primary'>
-            <PiFileMagnifyingGlass></PiFileMagnifyingGlass>
-          </button>
-          <button className='btn btn-square hover:bg-primary'>
-            <FiEdit></FiEdit>
-          </button>
-          <button className='btn btn-square hover:bg-primary'>
-            <FaTrashCan></FaTrashCan>
-          </button>
-        </td>
-      </tr>
-      )}
-
-    </tbody>
-  </table>
-</div>
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th>Payment Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.cost}</td>
+                <td>
+                  <button className="btn btn-square hover:bg-primary">
+                    <PiFileMagnifyingGlass></PiFileMagnifyingGlass>
+                  </button>
+                  <button className="btn btn-square hover:bg-primary mx-2">
+                    <FiEdit></FiEdit>
+                  </button>
+                  <button
+                    onClick={() => handleParcelDelete(parcel._id)}
+                    className="btn btn-square hover:bg-primary"
+                  >
+                    <FaTrashCan></FaTrashCan>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyParcel
+export default MyParcel;
