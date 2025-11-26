2a2f2cdf8ea08689f3a54dbc890b865121fd0537
mehedi hasan <mhshohan04@gmail.com>
2025-11-26 15:32:37 +0600

    Update parcel info after payment is confirmed

--- src/pages/dashboard/PaymentCancelled.jsx
+++ src/pages/dashboard/PaymentCancelled.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const PaymentCancelled = () => {
   return (
     <div>
       <h2 className='text-red-500 text-4xl text-center font-semibold'>PaymentCancelled</h2>
+      <Link to='/dashboard/my-parcels'>
+        <button className='btn btn-error'>Try Again</button>
+      </Link>
     </div>
   )
 }
