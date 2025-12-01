d54e9f24dde0e5b2e3a0a67690a75c9819689b55
mehedi hasan <mhshohan04@gmail.com>
2025-12-01 10:52:11 +0600

    ssign Riders and update delivery status with rider work status

--- src/pages/dashboard/AssignRider.jsx
+++ src/pages/dashboard/AssignRider.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AssignRider = () => {
   const [selectedParcel, setSelectedParcel] = useState(null);
   const axiosSecure = useAxiosSecure();
-  const riderModalRef =  useRef();
+  const riderModalRef = useRef();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch: parcelsRefetch } = useQuery({
     queryKey: ["parcels", "pending-pickup"],
     queryFn: async () => {
       const res = await axiosSecure.get(
@@ -17,20 +18,45 @@ const AssignRider = () => {
     },
   });
 
+  const { data: riders = [] } = useQuery({
+    queryKey: ["riders", selectedParcel?.senderDistrict, "available"],
+    enabled: !!selectedParcel,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/riders?status=approved&district=${selectedParcel.senderDistrict}&workStatus=available`
+      );
+      return res.data;
+    },
+  });
 
-    const { data: riders = []} = useQuery({
-      queryKey: ['riders', selectedParcel?.senderDistrict , 'available'],
-      enabled: !!selectedParcel,
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/riders?status=approved&district=${selectedParcel.senderDistrict}&workStatus=available`);
-        return res.data;
-      }
-    })
-
-  const openAssignRiderModal = parcel => {
+  const openAssignRiderModal = (parcel) => {
     setSelectedParcel(parcel);
     riderModalRef.current.showModal();
-  }
+  };
+
+  const handleAssignRider = (rider) => {
+    const riderAssignInfo = {
+      riderId: rider._id,
+      riderEmail: rider.email,
+      riderName: rider.name,
+      parcelId: selectedParcel._id,
+    };
+    axiosSecure
+      .patch(`/parcels/${selectedParcel._id}`, riderAssignInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          riderModalRef.current.close();
+          parcelsRefetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Rider has been assigned`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
   return (
     <div>
@@ -59,46 +85,54 @@ const AssignRider = () => {
                 <td>
                   <button
                     onClick={() => openAssignRiderModal(parcel)}
-                  className="btn btn-primary">Assign Rider</button>
+                    className="btn btn-primary"
+                  >
+                    Assign Rider
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <dialog ref={riderModalRef} className="modal modal-bottom sm:modal-middle">
+      <dialog
+        ref={riderModalRef}
+        className="modal modal-bottom sm:modal-middle"
+      >
         <div className="modal-box">
           <h3 className="font-bold text-lg">Riders: {riders.length}</h3>
 
-            <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>District</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        riders.map((rider, index) => <tr key={rider._id}>
-        <th>{index+1}</th>
-        <td>{rider.riderName}</td>
-        <td>{rider.riderEmail}</td>
-        <td>{rider.district}</td>
-        <td>
-          <button className="btn">Assign</button>
-        </td>
-      </tr>
-
-        )
-      }
-
-    </tbody>
-  </table>
-</div>
+          <div className="overflow-x-auto">
+            <table className="table table-zebra">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>District</th>
+                </tr>
+              </thead>
+              <tbody>
+                {riders.map((rider, index) => (
+                  <tr key={rider._id}>
+                    <th>{index + 1}</th>
+                    <td>{rider.riderName}</td>
+                    <td>{rider.riderEmail}</td>
+                    <td>{rider.district}</td>
+                    <td>
+                      <button
+                        onClick={() => handleAssignRider(rider)}
+                        className="btn"
+                      >
+                        Assign
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
           <div className="modal-action">
             <form method="dialog">
